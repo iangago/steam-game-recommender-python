04d66c01c9faa4e3e12664c79b6478e6416edf48
iangago <iangago20@gmail.com>
2026-03-13 14:25:44 -0300

    feat: library has all the functions that get the api reponse filter it and turns it into a array containing the user library as well as all the ids of the games in the library

---
+++ library.py
@@ -0,0 +1,38 @@
+import steam_api
+
+#INTENDED STRUCTURE
+#library = [
+#   {
+#    'name': "Hades",
+#    'game_id': 435
+#    'playtime': 5400}}
+#    'genres': ["Action", "Roguelike"]
+#   }
+#]
+
+def build_library(steam_id, api_key):
+    #gets the games from the function
+    games = steam_api.get_owned_games(steam_id, api_key)
+
+    library_list = []
+
+    #iterates trough each game
+    for game in games:
+        #builds the structure for the dictionary
+        library = {'name': game['name'],
+                   'game_id': game['appid'],
+                   'playtime': game['playtime_forever'],
+                   'genres': steam_api.get_game_details(game['appid'])}
+        
+        #adds dictionary to main library
+        library_list.append(library)
+    
+    return library_list
+
+def library_games_id(library):
+    library_ids = []
+    #gets every games id and adds it to the list
+    for dic in library:
+        library_ids.append(dic['game_id'])
+
+    return library_ids
