4658fcaa4e7c90d7d31bad31c5f5ea39b57b8dbb
iangago <iangago20@gmail.com>
2026-03-14 17:54:11 -0300

    feat: added loading the cache at the beggining of the program and saving the cache

---
+++ ui/terminal.py
@@ -0,0 +1,56 @@
+import steam.library as library
+import utils
+import core.analyzer as analyzer
+import core.recommender as recommender
+import steam.steam_api as steam_api
+import storage.cache as cache
+
+def print_terminal(api_key):
+    game_cache = cache.load_cache()
+
+    print("\nSTEAM GAME RECOMMENDER")
+    print("----------------------")
+
+    steam_id = input("\nEnter your SteamID64: ")
+
+    #make user library
+    print("\nAnalyzing Steam Library...")
+    user_library = library.build_library(steam_id, api_key, game_cache)
+
+    user_library_games_ids = library.library_games_id(user_library)
+
+    user_top_genres = utils.filter_genre_list(analyzer.top_genres(user_library))
+
+    weekly_top_100 = steam_api.get_top_100()
+    print("\nSearching Game Recommendations...")
+    user_recommended_games = recommender.recommend_games(weekly_top_100, user_top_genres, user_library_games_ids, game_cache)
+
+
+    #print user library basic information
+    print(f"\nTotal Games: {len(user_library)}")
+    print(f"Total Playtime: {utils.get_total_hours(user_library):.1f} hours")
+
+    #print top 3 genres
+    print("\nTop Genres")
+    i = 1
+    for genre in user_top_genres:
+        print(f"{i}. {genre}")
+        i += 1      
+
+    #print recommended games
+    #     Game Rec Structure
+    #1. Hades II
+    #   Score: 8
+    #   Genres matched: Roguelike, Action
+    #   Steam Rank: #12
+
+    i = 1
+    print("\nRecommended Games")
+    for game in user_recommended_games:
+        print(f"{i}. {game['name']}")
+        print(f"Score: {game['score']}")
+        print(f"Genres Matched: {game['genres_matched']}")
+        print(f"Steam Rank: #{game['steam_rank']}\n")
+        i += 1
+
+    cache.save_cache(game_cache)
